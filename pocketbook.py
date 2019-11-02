c424fec9fad8c82e74af69e70d563bd1a159a4da
Iain Funnell <yowie@MacBook.localdomain>
2019-11-02 12:49:46 +1100

    adding in basic epub and email functionality

--- pocketbook.py
+++ pocketbook.py
@@ -1,5 +1,9 @@
 import json
 import requests
+import yagmail
+import ebooklib
+from ebooklib import epub
+
 
 api_base_url = "https://getpocket.com/v3/get"
 api_auth_url = "https://getpocket.com/v3/oauth/request"
@@ -39,20 +43,76 @@ def do_get_articles():
     print(data)
 
 def do_load_config():
+    global pocket_conf
+    global kindle_conf
     with open('config.json') as config_file:
-        data = json.load(config_file)
-    return data
+        pocket_conf = json.load(config_file)
+    
+    with open('kindleconfig.json') as config_file:
+        kindle_conf = json.load(config_file)
+
     
 def do_save_config():
     with open('config.json', 'w') as outfile:
         json.dump(pocket_conf, outfile)
 
 
-pocket_conf = do_load_config()
+def do_create_epub():
+    book = epub.EpubBook()
+
+    # set metadata
+    book.set_identifier('id123456')
+    book.set_title('Sample book')
+    book.set_language('en')
+
+    book.add_author('Author Authorowski')
+    book.add_author('Danko Bananko', file_as='Gospodin Danko Bananko', role='ill', uid='coauthor')
+
+    # create chapter
+    c1 = epub.EpubHtml(title='Intro', file_name='chap_01.xhtml', lang='hr')
+    c1.content=u'<h1>Intro heading</h1><p>Zaba je skocila u baru.</p>'
+
+    # add chapter
+    book.add_item(c1)
+
+    # define Table Of Contents
+    book.toc = (epub.Link('chap_01.xhtml', 'Introduction', 'intro'),
+                (epub.Section('Simple book'),
+                (c1, ))
+                )
+
+    # add default NCX and Nav file
+    book.add_item(epub.EpubNcx())
+    book.add_item(epub.EpubNav())
+
+    # define CSS style
+    style = 'BODY {color: white;}'
+    nav_css = epub.EpubItem(uid="style_nav", file_name="style/nav.css", media_type="text/css", content=style)
+
+    # add CSS file
+    book.add_item(nav_css)
+
+    # basic spine
+    book.spine = ['nav', c1]
+
+    # write to the file
+    epub.write_epub('test.epub', book, {})
+
+def do_send_to_kindle():
+
+yag = yagmail.SMTP('pocketbookindle@gmail.com')
+contents = [
+    "This is the body, and here is just text http://somedomain/image.png",
+    "You can find an audio file attached.", 'test.epub'
+]
+yag.send(kindle_conf['email'], 'subject', contents)
+
+
+do_load_config()
 if not 'access_token' in pocket_conf:
     account_info = get_request_token()
     do_auth_redirect()
     do_convert_auth_token()
 do_get_articles()
-
-
+do_create_epub()
+#do_send_to_kindle()
